1db31dc836cd37351345e3006892aa8b80282eea
fryk06 <fryk06@gmail.com>
2021-08-15 01:34:44 +0200

    Adding Log4j2

--- src/main/java/com/cbtl/Application.java
+++ src/main/java/com/cbtl/Application.java
@@ -1,7 +1,11 @@
 package com.cbtl;
 
-public class Application {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Application {
+    public static final Logger logger = LogManager.getLogger(Application.class);
+    public static void main(String[] args) {
+        logger.info("Test message");
     }
 }
